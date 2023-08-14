42ab75ac6a731eed055936bf2ad31c43b7ca4e04
tarunmalik330 <maliktarun2005@gmail.com>
2023-08-14 09:11:50 +0530

    comment

--- js/doc.js
+++ js/doc.js
@@ -1,6 +1,6 @@
 let p = document.querySelector("p")
 let button = document.querySelector("button")
-let button1 = document.querySelector("button")
+// let button1 = document.querySelector("button")
 
 button.addEventListener("click", function () {
     p.innerHTML = +p.innerHTML + 1
